4da14ce0e54f2acf8c4b4c671eb28d7493095889
TergaGamesTeam <mr.max.kh@gmail.com>
2023-11-12 18:09:37 +0700

    #5 Начат переход на nunjucks

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const nunjucks = require('nunjucks')
 const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express()
 const $ = require('jquery');
+
+let commands = []
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/game.html'))
-  console.log("\x1b[34m\x1b[1m" + "Page game.html requested" + "\x1b[0m");
+  res.render('game.njk')
+  console.log("\x1b[34m\x1b[1m" + "Page game.njk requested" + "\x1b[0m");
 });
 
 router.post('/', function(req, res) {
-  const userInput = req.body.inpfield;
-  const comdec = "command detected"
-  res.send(userInput);
+  commands.append(req.body.inpfield);
 });
-
 
 module.exports = router;
